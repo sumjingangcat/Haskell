fa11b6f9fbaa20a3327b560ed06bfd11eef2d79d
sumjingangcat <oj47191@gmail.com>
2023-07-01 01:16:41 +0900

    [29p] Ch3 Type : initial commit

---
+++ type.hs
@@ -0,0 +1,10 @@
+-- Ch3 Type
+
+-- Type Description Examples
+-- Bool Boolean False, True
+-- Char character ’h’, ’7’
+-- String string "101 N. College Ave."
+-- Int small integer 42
+-- Integer arbitrary integer 18446744073709551616
+-- Float single-precision floating point 0.33333334
+-- Double double-precision floating point 0.3333333333333333
